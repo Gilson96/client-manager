01aa2f9992ba37db182bf06398d81032929a653e
Gilson96 <grafael99@gmail.com>
2021-04-16 16:31:20 +0100

    MySQL database created

--- database/migrations/2021_04_16_150315_create_clients_table.php
+++ database/migrations/2021_04_16_150315_create_clients_table.php
@@ -6,24 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateClientsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+    // run migrations
     public function up()
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->string("first_name", 50);
+            $table->string("last_name", 50);
+            $table->string("username", 20);
+            $table->boolean("dark_mode");
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    // reverse migration
     public function down()
     {
         Schema::dropIfExists('clients');

b016f14eec0be385fb2110db4ed467781b368030
Gilson96 <grafael99@gmail.com>
2021-04-16 16:30:21 +0100

    Resource created, to control what response comes out

---
+++ app/Http/Resources/API/ClientResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\API;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ClientResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        // just show the clients properties wanted
+        // $this represents the current client
+        return [
+            "id" => $this->id,
+            "first_name" => $this->first_name,
+            "last_name" => $this->last_name,
+            "username" => $this->username,
+        ];
+    }
+}

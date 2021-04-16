2715af4b04710b9d90f91d6eae2877158d36af06
Gilson96 <grafael99@gmail.com>
2021-04-16 19:31:24 +0100

    Demo

--- public/index.php
+++ public/index.php
@@ -54,7 +54,3 @@ $response = tap($kernel->handle(
 
 $kernel->terminate($request, $response);
 
-$app->get('/cowsay', function() use($app) {
-    $app['monolog']->addDebug('cowsay');
-    return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
-  });
